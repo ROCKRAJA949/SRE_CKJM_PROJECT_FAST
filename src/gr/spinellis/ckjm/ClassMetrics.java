28bddc80cbbbf0a3568c625bf366c28c816e25aa
Noureen Fatima <karissa.kaal@gmail.com>
2024-05-07 00:29:09 +0500

    ClassMetrics and ClassMetricsContainer Refactoring
    Extracted Two Classes
    - CoupledClasses
    - Metrics
    Code Readability

--- src/gr/spinellis/ckjm/ClassMetrics.java
+++ src/gr/spinellis/ckjm/ClassMetrics.java
@@ -29,83 +29,74 @@ import java.util.HashSet;
  * @author <a href="http://www.spinellis.gr">Diomidis Spinellis</a>
  */
 public class ClassMetrics {
-    /** Weighted methods per class */
-    private int wmc;
-    /** Number of children */
-    private int noc;
-    /** Response for a Class */
-    private int rfc;
-    /** Coupling between object classes */
-    private int cbo;
-    /** Depth of inheritence tree */
-    private int dit;
-    /** Lack of cohesion in methods */
-    private int lcom;
-    /** Number of public methods */
-    private int npm;
-    /** True if the class has been visited by the metrics gatherer */
+    private final CoupledClasses coupledClasses = new CoupledClasses();
+    private final Metrics metrics = new Metrics();
     private boolean visited;
-    /** True if the class is public */
     private boolean isPublicClass;
-    /** Coupled classes: classes that use this class */
-    private HashSet<String> afferentCoupledClasses;
 
-    /** Default constructor. */
     ClassMetrics() {
-	wmc = 0;
-	noc = 0;
-	cbo = 0;
-	npm = 0;
+        metrics.wmc = 0;
+        metrics.noc = 0;
+        metrics.setCbo(0);
+        metrics.npm = 0;
 	visited = false;
-	afferentCoupledClasses = new HashSet<String>();
+        coupledClasses.afferentCoupledClasses = new HashSet<String>();
     }
 
-    /** Increment the weighted methods count */
-    public void incWmc() { wmc++; }
-    /** Return the weighted methods per class metric */
-    public int getWmc() { return wmc; }
-
-    /** Increment the number of children */
-    public void incNoc() { noc++; }
-    /** Return the number of children */
-    public int getNoc() { return noc; }
-
-    /** Increment the Response for a Class */
-    public void setRfc(int r) { rfc = r; }
-    /** Return the Response for a Class */
-    public int getRfc() { return rfc; }
-
-    /** Set the depth of inheritence tree metric */
-    public void setDit(int d) { dit = d; }
-    /** Return the depth of the class's inheritance tree */
-    public int getDit() { return dit; }
-
-    /** Set the coupling between object classes metric */
-    public void setCbo(int c) { cbo = c; }
-    /** Return the coupling between object classes metric */
-    public int getCbo() { return cbo; }
-
-    /** Return the class's lack of cohesion in methods metric */
-    public int getLcom() { return lcom; }
-    /** Set the class's lack of cohesion in methods metric */
-    public void setLcom(int l) { lcom = l; }
-
-    /** Return the class's afferent couplings metric */
-    public int getCa() { return afferentCoupledClasses.size(); }
-    /** Add a class to the set of classes that depend on this class */
-    public void addAfferentCoupling(String name) { afferentCoupledClasses.add(name); }
-
-    /** Increment the number of public methods count */
-    public void incNpm() { npm++; }
-    /** Return the number of public methods metric */
-    public int getNpm() { return npm; }
-
-    /** Return true if the class is public */
+    //getters
+    public boolean isVisited() { return visited; }
+    public int getWmc() {
+        return metrics.getWmc();
+    }
+    public int getNoc() {
+        return metrics.getNoc();
+    }
+    public int getRfc() {
+        return metrics.getRfc();
+    }
+    public int getDit() {
+        return metrics.getDit();
+    }
+    public int getCbo() {
+        return metrics.getCbo();
+    }
+    public int getLcom() {
+        return metrics.getLcom();
+    }
+    public int getCa() { return coupledClasses.afferentCoupledClasses.size(); }
+    public int getNpm() {
+        return metrics.getNpm();
+    }
     public boolean isPublic() { return isPublicClass; }
-    /** Call to set the class as public */
-    public void setPublic() { isPublicClass = true; }
 
-    /** Return true if the class name is part of the Java SDK */
+    //setters
+    public void setLcom(int l) {
+        metrics.setLcom(l);
+    }
+    public void setCbo(int c) {
+        metrics.setCbo(c);
+    }
+    public void setDit(int d) {
+        metrics.setDit(d);
+    }
+    public void setRfc(int r) {
+        metrics.setRfc(r);
+    }
+    public void incNoc() {
+        metrics.incNoc();
+    }
+    public void incWmc() {
+        metrics.incWmc();
+    }
+    public void setVisited() { visited = true; }
+    public void setPublic() { isPublicClass = true; }
+    public void incNpm() {
+        metrics.incNpm();
+    }
+    public void addAfferentCoupling(String name) {
+        coupledClasses.addAfferentCoupling(name);
+    }
+
     public static boolean isJdkClass(String s) {
 	return (s.startsWith("java.") ||
 		s.startsWith("javax.") ||
@@ -114,26 +105,16 @@ public class ClassMetrics {
 		s.startsWith("org.xml.sax."));
     }
 
-    /** Return the 6 CK metrics plus Ce as a space-separated string */
     public String toString() {
 	return (
-		wmc +
-		" " + getDit() +
-		" " + noc +
-		" " + cbo +
-		" " + rfc +
-		" " + lcom +
+            metrics.getWmc() +
+		" " + metrics.getDit() +
+		" " + metrics.getNoc() +
+		" " + metrics.getCbo() +
+		" " + metrics.getRfc() +
+		" " + metrics.getLcom() +
 		" " + getCa()+
-		" " + npm);
+		" " + metrics.getNpm());
     }
 
-    /** Mark the instance as visited by the metrics analyzer */
-    public void setVisited() { visited = true; }
-    /**
-     * Return true if the class has been visited by the metrics analyzer.
-     * Classes may appear in the collection as a result of some kind
-     * of coupling.  However, unless they are visited and analyzed,
-     * we do not want them to appear in the output results.
-     */
-    public boolean isVisited() { return visited; }
 }
